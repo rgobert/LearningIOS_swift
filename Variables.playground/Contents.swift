235ee4340dda4ba9ed066ede277fd10d4255b323
rgobert <rgobert@gmail.com>
2017-01-29 23:01:31 +0100

    Commit ios programs

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number:Int = 20
+var decimal:Float = 50.5
+var longdecimal:Double = 59.9483948343
+
+let age:Int = 50
+
+
+
+var f1 = 2, f2 = 3, f4 = 5
+var testType:String, testType2:String, testType3:String
+testType = "j'écris ce que je veux avec un chiffre \(f4)"
+
+
+var account:Int = 50
+
+var sum = account + Int(decimal)
+
+var str2 = "charlie est les \(sum) chocolats"
+
+
+var a = 15
+var b = 5
+var c = a+b
+var d = a*b
+var e = a-b
+var f = a/b
+
+var g=1, h=2, i=3, j=4
+let k=5, l=6, m=7
+
+var vardouble:Double
+var varInt:Int = 423
+var strHello = "Hello "
+var strWorld = "World"
+var strResult = strHello + strWorld
